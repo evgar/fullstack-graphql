91e093af2e9b4289b6d6d1704c06836846c116c3
evgar <eugene.garichev@gmail.com>
2020-01-11 02:30:37 +0200

    add apollo client tot prjct

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,29 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import { HttpLink } from 'apollo-link-http';
 import gql from 'graphql-tag'
 
-/**
- * Create a new apollo client and export as default
- */
+const link = new HttpLink({ uri: 'http://localhost:4000'});
+
+const cache = new InMemoryCache();
+
+const query = gql`
+      {
+          pets {
+            id
+          }
+      }
+`;
+
+const client = new ApolloClient(
+    {
+        link,
+        cache
+    }
+);
+
+client.query({query})
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+export default client;
